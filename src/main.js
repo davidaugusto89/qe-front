c3b45de74e26bc116194f9968b22b43371641c59
David Augusto Keller Haddad <davidhaddad@dealersites.com.br>
2023-08-18 09:19:36 -0300

    Instalando Vuetify + Sweetalert2

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).use(store).use(router).mount('#app')
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .use(VueSweetalert2)
+  .mount('#app')
